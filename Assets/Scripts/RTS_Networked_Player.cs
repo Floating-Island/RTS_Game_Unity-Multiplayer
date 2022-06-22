348016642fe23840f907ea87c49175bbf130e066
Alber <betomiku@gmail.com>
2022-06-22 01:46:00 -0300

    Fixes script name.

--- Assets/Scripts/RTS_Networked_Player.cs
+++ Assets/Scripts/RTS_Networked_Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RTS_Netoworked_Player : NetworkBehaviour
+public class RTS_Networked_Player : NetworkBehaviour
 {
 
 }
