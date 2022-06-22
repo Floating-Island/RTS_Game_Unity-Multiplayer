1274196587b69e4f221367d71a937135660e349c
Alber <betomiku@gmail.com>
2022-06-22 00:05:37 -0300

    Instantiates a unit spawner for the recently added player and replicates it.

--- Assets/Scripts/RTS_NetworkManager.cs
+++ Assets/Scripts/RTS_NetworkManager.cs
@@ -5,5 +5,14 @@ using UnityEngine;
 
 public class RTS_NetworkManager : NetworkManager
 {
-    
+    [SerializeField]
+    private GameObject unitSpawnerPrefab = null;
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Transform playerTransform = conn.identity.transform;
+        GameObject unitSpawner = Instantiate(unitSpawnerPrefab, playerTransform.position, playerTransform.rotation);
+        NetworkServer.Spawn(unitSpawner, conn);
+    }
 }
