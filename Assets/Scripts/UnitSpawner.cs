599d9e267d36371d7f40786ada971d25ea5b9a89
Alber <betomiku@gmail.com>
2022-06-21 23:03:32 -0300

    Sets class as Networked.

--- Assets/Scripts/UnitSpawner.cs
+++ Assets/Scripts/UnitSpawner.cs
@@ -1,18 +1,9 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitSpawner : MonoBehaviour
+public class UnitSpawner : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
