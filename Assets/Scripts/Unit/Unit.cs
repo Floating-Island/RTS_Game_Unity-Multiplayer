ea5e4aa9bd10bc600851fae1fbf19dd885d44629
Alber <betomiku@gmail.com>
2022-06-29 14:56:48 -0300

    Renames UnitEvents class to Unit.

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class UnitEvents : NetworkBehaviour
+public class Unit : NetworkBehaviour
 {
     [SerializeField]
     private Pointed_Movement unitMovement = null;
